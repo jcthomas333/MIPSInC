8de3675d6c19af9a6b74f53386706a218cff9c3d
jgoat <jcthomas4105@gamil.com>
2025-05-23 17:31:38 -0400

    frist

---
+++ mps.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("opinion one input file and output file");
+    printf("opinion two input file and output termmal");
+    printf("opinion three input termmal and output file");
+    printf("opinion four input termmal and output termmal");
+    
+    int x; x = 0;
+    printf("pick one: ");
+    
+    while(x<5 && x >0)
+    {
+        
+    }
+    return 0;
+}
